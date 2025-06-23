4fef164ef93e0fabb5bc7864a73fc3349c0a7666
Jan Rohleff <148617178+janrohleff@users.noreply.github.com>
2025-06-23 12:10:16 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -38,17 +38,17 @@ def p_x_z_compute(data, x_mean, res, lengths):
 def p_z_compute(z, z_pop, omega_pop):
     ln_pi = torch.log(2*torch.tensor(np.pi))
     p_z = 0.5*torch.sum((z-z_pop).pow(2)/omega_pop + omega_pop.log() + ln_pi)
+    
     return p_z  
 
 def q_z_x_compute(eps, sigma):
     ln_pi = torch.log(2*torch.tensor(np.pi))
     q_z_x = 0.5 * torch.sum(eps.pow(2) + ln_pi + 2*sigma.log())
-        
+     
     return q_z_x
 
 def p_z_compute_batch(z, z_pop, omega_pop):
     ln_pi = torch.log(2*torch.tensor(np.pi))
-   
     p_z = 0.5*torch.sum(((z-z_pop).pow(2)/omega_pop + omega_pop.log() + ln_pi), dim =1)
     return p_z   
 
@@ -61,12 +61,8 @@ def q_z_x_compute_batch(eps, sigma):
 def p_x_z_compute_lengths_batch(data, x_mean, res, lengths):
     a = res[0]
     b = res[1]
-    
-    # Berechne sigma
     sigma = a + b * x_mean  # Shape: [nbatch, nfeatures]
-
     p_x_z = torch.zeros(data.shape[0])
-
     for i in range(data.shape[0]):
         err = (data[i,:lengths[i]] - x_mean[i,:lengths[i]]) / sigma[i,:lengths[i]]
         p_x_z[i] = 0.5 * torch.sum(err.pow(2)) + 0.5 * lengths[i] * torch.log(2 * torch.tensor(np.pi)) + torch.log(sigma[i,:lengths[i]]).sum()
@@ -83,14 +79,12 @@ def LogLikelihood_linearization(z_pop_hat, omega_pop, res, data, phi, C, h, time
     # Functions 
     phi_normal = phi.clone().detach()
     phi0 = phi.clone().detach().requires_grad_(True)
-
     pred_x = Decoder(phi0, time, h)
     
     # Compute log likelihood
     mu = torch.zeros(N, data.shape[1])
     df_dphi = torch.zeros(N, data.shape[1], z_dim)
     ff = torch.zeros(N,data.shape[1])
-    
     for t in range(data.shape[1]):
         ff[:,t] = pred_x[:,t,0]
         output = ff[:,t].sum().backward(retain_graph=True)
@@ -99,7 +93,7 @@ def LogLikelihood_linearization(z_pop_hat, omega_pop, res, data, phi, C, h, time
         for zz in range(z_dim):
             mu[:,t] += phi0.grad[:,zz]*(z_pop[:,zz] - phi_normal[:,zz])
         phi0.grad.zero_()
-    
+
     LL = 0
     for i in range(N):
         g = res[0]+res[1]*ff[i,:lengths[i]]
@@ -108,7 +102,6 @@ def LogLikelihood_linearization(z_pop_hat, omega_pop, res, data, phi, C, h, time
         #dzf = torch.matmul(df_dphi[i], J_h[i])
         dzf = df_dphi[i, :lengths[i]]
         Gamma = torch.matmul(dzf, torch.matmul(omega_pop.diag(), dzf.T))+ tmp
-    
         LL += lengths[i]/2*torch.log(2*torch.tensor(np.pi))  +0.5*torch.linalg.slogdet(Gamma).logabsdet + 0.5*torch.matmul(data[i,:lengths[i],1] - mu[i, :lengths[i]], torch.linalg.solve(Gamma, data[i,:lengths[i],1] - mu[i,:lengths[i]]))
         
     return LL
@@ -119,7 +112,6 @@ def LogLikelihood_sample(M, z_pop_hat, omega_pop, res, data, mu, L, C, h, time, 
     z_pop = torch.zeros(nbatch, z_dim)
     for i in range(nbatch):
         z_pop[i] = torch.matmul(C[i], z_pop_hat)
-
     #########################################################
     # Compute true posterior
     #########################################################
@@ -131,8 +123,7 @@ def LogLikelihood_sample(M, z_pop_hat, omega_pop, res, data, mu, L, C, h, time, 
         eps = torch.randn(nbatch, z_dim)
         z_normal = mu + torch.matmul(L, eps.unsqueeze(-1)).squeeze(-1)
         z_samples[k] = z_normal 
-        pred_x = Decoder(z_normal, time, h)
-            
+        pred_x = Decoder(z_normal, time, h)    
         p_x_z = p_x_z_compute_lengths_batch(data[:,:,1].view(nbatch,data.shape[1],1) , pred_x, res, lengths)
         p_z  = p_z_compute_batch(z_normal, z_pop, omega_pop)
         q_z = q_z_x_compute_batch(eps, torch.diagonal(L, dim1=1, dim2=2))
@@ -140,10 +131,8 @@ def LogLikelihood_sample(M, z_pop_hat, omega_pop, res, data, mu, L, C, h, time, 
         weights_normalized = log_weights[:k+1,:] - torch.logsumexp(log_weights[:k+1,:], dim=0).view(1,nbatch)
         weights = torch.exp(weights_normalized)
 
-
     mu_true = torch.sum(weights.unsqueeze(-1) * z_samples[:k+1], dim=0)  
     diff = z_samples[:k+1] - mu_true
-
     outer = diff.unsqueeze(-1) @ diff.unsqueeze(-2)  
     weights_ = weights.unsqueeze(-1).unsqueeze(-1)  
     cov_true = torch.sum(weights_ * outer, dim=0)
@@ -155,20 +144,13 @@ def LogLikelihood_sample(M, z_pop_hat, omega_pop, res, data, mu, L, C, h, time, 
     for m in range(M):      
         eps = torch.randn(nbatch, z_dim)
         z_sample = mu_true + torch.matmul(L_true, eps.unsqueeze(-1)).squeeze(-1)
-        pred_x = Decoder(z_sample, time, h)
-       
+        pred_x = Decoder(z_sample, time, h)  
         p_x_z = p_x_z_compute_lengths_batch(data[:,:,1].view(nbatch,data.shape[1],1) , pred_x, res, lengths)
         p_z  = p_z_compute_batch(z_sample, z_pop, omega_pop)
         q_z = q_z_x_compute_batch(eps, torch.diagonal(L_true, dim1=1, dim2=2))
-
         log_w[m] = -(p_x_z + p_z - q_z)
-
         log_px = torch.logsumexp(log_w[:m+1], dim =0) - torch.log(torch.tensor(m+1))
-        #if m % 1000 == 0:
-        #    print('M', m, '2log_px', 2*log_px.sum())
-
-
-      
+ 
     return -log_px.sum(), - log_px
 
 def initalizeEncoder(iters, L_iter, Encoder, Decoder, data, data_in, z_dim, covariates_in, lengths, h, pop):
@@ -191,25 +173,19 @@ def initalizeEncoder(iters, L_iter, Encoder, Decoder, data, data_in, z_dim, cova
         z_normal, mu, L, log_sigma, eps = Encoder(data_in, covariates_in, lengths)
         # Decoder
         pred_x = Decoder(z_normal, data[:,:,0], h)
-
-
         # Update population parameters
         z_pop, omega_pop, a, _ = pop.update_pop(mu.detach(), L.detach(), pred_x.detach(), 0)
-
         # Compute loss
         p_x_z = p_x_z_compute(data[:,:,1].view(data.shape[0],data.shape[1],1), pred_x, [a,b], lengths)
         p_z  = p_z_compute(z_normal, z_pop, omega_pop)
         q_z = q_z_x_compute(eps, torch.diagonal(L, dim1=1, dim2=2))
         DKL = p_z - q_z 
-        elbo = p_x_z + 0.01 * DKL
-   
-            
+        elbo = p_x_z + 0.01 * DKL   
         elbo.backward()
         optimizer.step()
         # Reset gradient
         optimizer.zero_grad()
         if iter % L_iter == 0:
-
             print(f"Iteration {count + 1}/{iters}", end="\r")
             z_pop_iter_bi[count] = h(z_pop)
             omega_pop_iter_bi[count] = omega_pop.sqrt()
